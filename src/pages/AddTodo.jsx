3cf505370319bec988ed0059e634e510908b501d
homfarnam <homfarnam@gmail.com>
2021-01-23 20:04:58 +0330

    Change submit button style

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,6 +1,35 @@
 import { useToDoList, addTodo, generateTodo } from "../context"
 import { useHistory } from "react-router-dom"
 import { useState } from "react"
+import styled from "styled-components"
+
+export const Title = styled.h2`
+  font-variant: petite-caps;
+  font-size: 30px;
+`
+
+const Label = styled.label`
+  font-size: 18px;
+`
+
+export const SubmitButton = styled.button`
+  color: #494949 !important;
+  text-transform: uppercase;
+  text-decoration: none;
+  background: blanchedalmond;
+  padding: 5px;
+  border: 4px solid #494949 !important;
+  display: inline-block;
+  transition: all 0.4s ease 0s;
+  width: 100px;
+
+  :hover {
+    color: #ffffff !important;
+    background: #f6b93b;
+    border-color: black !important;
+    transition: all 0.4s ease 0s;
+  }
+`
 
 const AddTodo = () => {
   const [todo, setTodo] = useState(() => generateTodo())
@@ -21,15 +50,15 @@ const AddTodo = () => {
     setTodo((todo) => ({ ...todo, name }))
   }
   return (
-    <div className="new-todo">
-      <h2 className="title">Add New Todo</h2>
+    <div>
+      <Title>Add New Todo</Title>
       <form onSubmit={submitHandler} name="addTodo">
         <p>
-          <label htmlFor="name">Todo Name: </label>
+          <Label htmlFor="name">Todo Name: </Label>
           <input type="text" name="nam" id="name" onChange={changeHandler} />
         </p>
         <p>
-          <button type="submit">submit</button>
+          <SubmitButton type="submit">submit</SubmitButton>
         </p>
       </form>
     </div>
