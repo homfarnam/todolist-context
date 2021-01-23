bc0def59b8c702fc8bd031c73a5461455bc8814e
homfarnam <homfarnam@gmail.com>
2021-01-23 19:08:35 +0330

    Create main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { Route, Switch } from "react-router-dom"
+import { TodoListProvider } from "../../context"
+import NotFound from "../../pages/NotFound"
+import styled from "styled-components"
+
+const StyledMain = styled.main`
+  padding: 12px 12px;
+`
+const Main = ({ children }) => {
+  return (
+    <StyledMain>
+      <TodoListProvider>
+        <Switch>
+          {children}
+          <Route path="*" component={NotFound} exact />
+        </Switch>
+      </TodoListProvider>
+    </StyledMain>
+  )
+}
+
+export default Main
